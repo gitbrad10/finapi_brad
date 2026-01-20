adea3ada363eadff791bdacb90b3c3cbefb91d60
Brad Bell <youngandfree27@hotmail.com>
2026-01-20 03:53:59 -0600

    Initial Source code delivery

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import os
-import time
 from azure.storage.blob import BlobServiceClient
 from datetime import timezone
 
@@ -108,8 +107,3 @@ def list_csv_reports():
         return jsonify({"error": str(e)}), 500
 
 # IMPORTANT: No app.run() when deploying to Azure Linux App Service
-
-@app.route('/api/slow-endpoint')
-    def slow_endpoint():
-        time.sleep(5)
-        return "This was a slow response after 5 seconds"
